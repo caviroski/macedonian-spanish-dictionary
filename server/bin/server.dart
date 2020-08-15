65518a0f9729482d8ac0fb1cb20bbf8e31962073
Igor Caviroski <icavir@gmail.com>
2020-08-15 14:16:30 +0200

    read local json

--- server/bin/server.dart
+++ server/bin/server.dart
@@ -1,15 +1,22 @@
 import 'dart:io';
+import 'dart:convert';
 
 import 'package:args/args.dart';
 import 'package:shelf/shelf.dart' as shelf;
 import 'package:shelf/shelf_io.dart' as io;
+import 'package:shelf_router/shelf_router.dart';
 
 // For Google Cloud Run, set _hostname to '0.0.0.0'.
 const _hostname = 'localhost';
 
+var jsonPath = File('../assets/dictionary.json');
+var dictionaryJson = '';
+
 void main(List<String> args) async {
   var parser = ArgParser()..addOption('port', abbr: 'p');
   var result = parser.parse(args);
+  var app = Router();
+  var aaa = 22;
 
   // For Google Cloud Run, we respect the PORT environment variable
   var portStr = result['port'] ?? Platform.environment['PORT'] ?? '4500';
@@ -22,13 +29,30 @@ void main(List<String> args) async {
     return;
   }
 
-  var handler = const shelf.Pipeline()
-      .addMiddleware(shelf.logRequests())
-      .addHandler(_echoRequest);
+  app.get('/hello', (shelf.Request request) {
+    return shelf.Response.ok('hello-world');
+  });
 
-  var server = await io.serve(handler, _hostname, port);
+  app.get('/', (shelf.Request request) {
+    return shelf.Response.ok('hello-world $aaa');
+  });
+
+  var server = await io.serve(app.handler, _hostname, port);
   print('Serving at http://${server.address.host}:${server.port}');
 }
 
-shelf.Response _echoRequest(shelf.Request request) =>
-    shelf.Response.ok('Request for "${request.url}"');
+Future readJsonFile() async {
+  var jsonText = '';
+
+  final lines = utf8.decoder
+    .bind(jsonPath.openRead())
+    .transform(const LineSplitter());
+  try {
+    await for (var line in lines) {
+      jsonText = jsonText + line;
+    }
+    dictionaryJson = jsonDecode(jsonText);
+  } catch (_) {
+    print('error: $_');
+  }
+}
