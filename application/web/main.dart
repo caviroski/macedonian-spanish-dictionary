8c13da9d7d91c3345de6958b6db1fcb846b1f67a
Igor Caviroski <icavir@gmail.com>
2020-08-16 14:11:29 +0200

    input check

--- application/web/main.dart
+++ application/web/main.dart
@@ -1,5 +1,30 @@
 import 'dart:html';
 
 void main() {
-  
+  querySelector('.translate-button').onClick.listen(onTranslateClick); 
+}
+
+void onTranslateClick(MouseEvent event) {
+  var wordForTranslation = (querySelector('.word-input') as InputElement).value;
+  checkInput(wordForTranslation);
+}
+
+void checkInput(String word) {
+  if (word == '') {
+    (querySelector('.translation-display') as InputElement).value = 'Please enter word';
+  } else if (isCyrillicWord(word)) {
+    print('translate');
+  } else if (!isWord(word)) {
+    (querySelector('.translation-display') as InputElement).value = 'Only words accepted';
+  } else if (isWord(word)) {
+    (querySelector('.translation-display') as InputElement).value = 'Only Macedonian Cyrillic words accepted';
+  }
+}
+
+bool isWord(String word) {
+  return RegExp(r'^[a-zA-Z]+$').hasMatch(word);
+}
+
+bool isCyrillicWord(String word) {
+  return RegExp(r'^[аАбБвВгГдДѓЃеЕжЖзЗѕЅиИјЈкКлЛљЉмМнНњЊоОпПрРсСтТќЌуУфФхХцЦчЧџЏшШ]+$').hasMatch(word);
 }
